41362332ada4d8a9b6ea0c033daf74d64255a5f1
George Le <george.le.goldenowl@gmail.com>
2022-06-29 17:40:25 +0700

    update: main

--- assets/query.js
+++ assets/query.js
@@ -1,21 +1,20 @@
 !(function($) {
     'use strict';
     const tatoo = {
-    	elements: undefined,
-    	init: function() {
-    		this.elements = {
-    			home: {
-    				slide: $(document.body).find('.homepage-slide')
-    			}
-    		};
-
-    		document.addEventListener('DOMContentLoaded', function() {
-    			console.log(this.elements);
-    		});
-    	},
-    	fetchData: function() {
-    		console.log(this);
-    	}
+        elements: undefined,
+        init: function() {
+            document.addEventListener('DOMContentLoaded', function() {
+                this.elements = {
+                    home: {
+                        slide: document.querySelector('.homepage-slide')
+                    }
+                };
+                console.log(this.elements);
+            });
+        },
+        fetchData: function() {
+            console.log(this);
+        }
     };
     tatoo.init();
 })(jQuery);
