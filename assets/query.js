4c4d87f297fe7ca40cfc975bca29a3906deb894a
George Le <george.le.goldenowl@gmail.com>
2022-06-29 15:59:24 +0700

    update: main

---
+++ assets/query.js
@@ -0,0 +1,16 @@
+!(function($) {
+    'use strict';
+    class LocalizationQuery() {
+        constructor() {
+            this.elements = {
+                homeslide: $('#homepage-slide')
+            };
+            this.elements.homeslide.addEventListener('onload', this.fetchData.bind(this));
+        }
+
+        fetchData() {
+            console.log(this)
+        }
+    }
+    new LocalizationQuery();
+})(jQuery);
