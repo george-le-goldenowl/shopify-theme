2c6b3b9a7361c0d5dad5753ca17d31e22607a38c
George Le <george.le.goldenowl@gmail.com>
2022-06-29 16:18:32 +0700

    update: main

--- assets/query.js
+++ assets/query.js
@@ -7,7 +7,7 @@
     			homeslide: document.querySelector('.homepage-slide')
     		};
 
-    		this.elements.homeslide.addEventListener('load', this.fetchData.bind(this));
+    		this.elements.homeslide.addEventListener('DOMContentLoaded', this.fetchData.bind(this));
     	},
     	fetchData: function() {
     		console.log(this);
